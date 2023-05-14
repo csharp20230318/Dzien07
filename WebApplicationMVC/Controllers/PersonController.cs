a4c874240a033b612f6c944214023790ca99cd3b
kurs <kurs@DESKTOP-V3HDKPQ>
2023-05-14 13:05:10 +0200

    update

---
+++ WebApplicationMVC/Controllers/PersonController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationMVC.Models;
+
+namespace WebApplicationMVC.Controllers
+{
+    public class PersonController : Controller
+    {
+        static IList<Person> peopleList = new List<Person>
+        {
+            new Person {PersonId = 1 , PersonName= "Jan Kowalski", PersonAge=33},
+            new Person {PersonId = 6 , PersonName= "Marian Kowalski", PersonAge=44},
+            new Person {PersonId = 4 , PersonName= "Jan Rokita", PersonAge=39},
+            new Person {PersonId = 3 , PersonName= "Zenon Martyniuk", PersonAge=65},
+            new Person {PersonId = 5 , PersonName= "Agnieszka Osiecka", PersonAge=21},
+            new Person {PersonId = 2 , PersonName= "Jacek Cygan", PersonAge=33},
+        };
+
+        public IActionResult Index()
+        {
+            return View(peopleList.OrderBy(x => x.PersonId).ToList());
+        }
+
+        public IActionResult Edit(int Id)
+        {
+            var person = peopleList.Where(x => x.PersonId == Id).FirstOrDefault();
+            if (person==null)
+            {
+                return NotFound();
+            }
+            return View(person);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Person person)
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplicationMVC/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace WebApplicationMVC.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        
+        [DisplayName("Person name")]
+        public string PersonName { get; set; } = "";
+
+		[DisplayName("Person age")]
+		public int PersonAge { get; set; }
+    }
+}
